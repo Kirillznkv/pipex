3bd2cb74cb02fb3e13c4e223a33f79b55571a929
kshanti_home <Zenich99@gmail.com>
2021-08-28 17:35:28 +0300

    fucking scanf test

--- srcs/commands.c
+++ srcs/commands.c
@@ -6,11 +6,12 @@
 /*   By: kshanti <kshanti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/27 02:02:19 by kshanti           #+#    #+#             */
-/*   Updated: 2021/08/02 11:25:04 by kshanti          ###   ########.fr       */
+/*   Updated: 2021/08/20 00:38:15 by kshanti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
+#include "stdio.h"//
 
 //
 void	output(char **a)
@@ -30,30 +31,47 @@ int start_commands(t_commands *commands, char **env)
 	int		fd[2];
 
 	pipe(fd);
+	printf("%d %d\n", fd[0], fd[1]);
 	if (!find_exec(commands->cmd1, env))
 		return (error("Error: exec not found\n") && struct_free(commands, NULL, NULL));
 	if (!find_exec(commands->cmd2, env))
 		return (error("Error: exec not found\n") && struct_free(commands, NULL, NULL));
 
-    dup2(commands->fd_in, 0);
-	dup2(fd[1], 1);
 	pd1 = fork();
 	if (!pd1)
 	{
+		close(0);
+		close(1);
+		dup2(commands->fd_in, 0);
+		dup2(fd[1], 1);
     	execve((commands->cmd1)[0], commands->cmd1, env);
 		exit(1);//
 	}
 	waitpid(pd1, NULL, 0);
 
-    dup2(fd[0], 0);
-    dup2(commands->fd_out, 1);
 	pd2 = fork();
 	if (!pd2)
 	{
-    	execve((commands->cmd2)[0], commands->cmd2, env);
-		exit(1);//
+		close(0);
+		// close(1);
+		dup2(fd[0], 0);
+		// dup2(commands->fd_out, 1);
+		char ch;
+		while (scanf("%c", &ch) == 1)
+		{
+			printf("%c", ch);
+			printf("a");
+		}
+		write(1, "---------------\n", 17);
+    	// execve((commands->cmd2)[0], commands->cmd2, env);
+		printf("wtf-----\n");
+		// exit(1);//
+		printf("wtf#######n");
 	}
+	usleep(10000);
+	printf("wtf\n");
 	waitpid(pd2, NULL, 0);
+	printf("wtf2\n");
 
     return (0);
 }
