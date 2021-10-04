e686088ba12449a122b5e9d6cd07513290338b43
Kenda Shanti <kshanti@si-b1.kzn.21-school.ru>
2021-10-04 19:54:46 +0300

    NORM gnl and includes

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: kshanti <kshanti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/27 00:48:09 by kshanti           #+#    #+#             */
-/*   Updated: 2021/10/04 19:15:07 by kshanti          ###   ########.fr       */
+/*   Updated: 2021/10/04 19:52:07 by kshanti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,21 +23,21 @@ typedef struct s_cmd
 {
 	char		**argv;
 }				t_cmd;
-typedef struct  s_commands
+typedef struct s_commands
 {
 	int			numders_cmd;
-    int         fd_in;
-    int         fd_out;
-    t_cmd		*cmd;
+	int			fd_in;
+	int			fd_out;
+	t_cmd		*cmd;
 	int			**fd;
 	pid_t		*pid;
-}               t_commands;
+}				t_commands;
 /*------------------------Utils-----------------------------------------------*/
 int			error(char *str);
-void	    skip_spases(char *str, int *i);
+void		skip_spases(char *str, int *i);
 int			free_struct(t_commands *commands);
 int			free_masInt(int **arr, int n);
-int		    free_mas(char **argv);
+int			free_mas(char **argv);
 /*------------------------Base_Utils------------------------------------------*/
 int			ft_strlen(char *str);
 char		*ft_strdup(char *s);
@@ -45,7 +45,7 @@ int			ft_strlcat(char *dst, char *src, int size);
 char		*ft_strjoin(char *s1, char *s2);
 size_t		ft_strlcpy(char *dst, const char *src, size_t size);
 int			ft_strncmp(const char *str1, const char *str2, size_t num);
-char	    *ft_substr(char *s, unsigned int start, size_t len);
+char		*ft_substr(char *s, unsigned int start, size_t len);
 int			isEquals(char *str1, char *str2);
 /*------------------------Init------------------------------------------------*/
 int			set_commands(t_commands *commands, char **argv);
@@ -54,6 +54,6 @@ int			start_commands(t_commands *commands, char **env);
 /*------------------------Split-----------------------------------------------*/
 char		**ft_split(char const *s, char c);
 /*------------------------Find_exec-------------------------------------------*/
-char	    *find_exec(char **str, char **env);
+char		*find_exec(char **str, char **env);
 
 #endif
--- includes/pipex_bonus.h
+++ includes/pipex_bonus.h
@@ -6,12 +6,12 @@
 /*   By: kshanti <kshanti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/27 00:48:09 by kshanti           #+#    #+#             */
-/*   Updated: 2021/10/04 19:14:57 by kshanti          ###   ########.fr       */
+/*   Updated: 2021/10/04 19:53:53 by kshanti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-# define PIPEX_H
+#ifndef PIPEX_BONUS_H
+# define PIPEX_BONUS_H
 
 # include "stdlib.h"
 # include "stdio.h"//
@@ -19,27 +19,26 @@
 # include "fcntl.h"
 # include <sys/wait.h>
 
-int	get_next_line(int fd, char **line);
-
 typedef struct s_cmd
 {
 	char		**argv;
 }				t_cmd;
-typedef struct  s_commands
+typedef struct s_commands
 {
 	int			numders_cmd;
-    int         fd_in;
-    int         fd_out;
-    t_cmd		*cmd;
+	int			fd_in;
+	int			fd_out;
+	t_cmd		*cmd;
 	int			**fd;
 	pid_t		*pid;
-}               t_commands;
+}				t_commands;
+int			get_next_line(int fd, char **line);
 /*------------------------Utils-----------------------------------------------*/
 int			error(char *str);
-void	    skip_spases(char *str, int *i);
+void		skip_spases(char *str, int *i);
 int			free_struct(t_commands *commands);
 int			free_masInt(int **arr, int n);
-int		    free_mas(char **argv);
+int			free_mas(char **argv);
 /*------------------------Base_Utils------------------------------------------*/
 int			ft_strlen(char *str);
 char		*ft_strdup(char *s);
@@ -47,7 +46,7 @@ int			ft_strlcat(char *dst, char *src, int size);
 char		*ft_strjoin(char *s1, char *s2);
 size_t		ft_strlcpy(char *dst, const char *src, size_t size);
 int			ft_strncmp(const char *str1, const char *str2, size_t num);
-char	    *ft_substr(char *s, unsigned int start, size_t len);
+char		*ft_substr(char *s, unsigned int start, size_t len);
 int			isEquals(char *str1, char *str2);
 /*------------------------Init------------------------------------------------*/
 int			set_commands(t_commands *commands, char **argv);
@@ -56,6 +55,6 @@ int			start_commands(t_commands *commands, char **env);
 /*------------------------Split-----------------------------------------------*/
 char		**ft_split(char const *s, char c);
 /*------------------------Find_exec-------------------------------------------*/
-char	    *find_exec(char **str, char **env);
+char		*find_exec(char **str, char **env);
 
 #endif
